ddf5c5c650ab2d45b6ea8601b2fd071b372d4034
Riccardo Scalco <riccardo.scalco@gmail.com>
2016-01-23 18:46:36 +0100

    fix

--- main.js
+++ main.js
@@ -1,25 +1,4 @@
-import { moveForward, moveBackward } from './lib/actionsCreators'
-import { store } from './lib/store'
-import { init } from './lib/init'
-import { update } from './lib/update'
+import './lib/init'
+import './lib/subscriptions'
+import './lib/events'
 
-// Draw SVG
-init()
-
-// Render circle
-update()
-
-// Every time the state changes, log it
-let unsubscribe = store.subscribe(() =>
-  //console.log(store.getState())
-  update()
-)
-
-// Dispatch some actions
-// store.dispatch(moveForward())
-// store.dispatch(moveForward())
-// store.dispatch(moveBackward())
-
-document.addEventListener('click', () => {
-  store.dispatch(moveForward())
-})
